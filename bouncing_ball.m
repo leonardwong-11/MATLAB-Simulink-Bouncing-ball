938cd059baec58ac701d8dfc5b6427dda9cef0ad
Leonard Wong <leonard.11.wong@gmail.com>
2026-07-09 11:52:55 +0100

    compelte model in matlab and uploaded the figure

--- bouncing_ball.m
+++ bouncing_ball.m
@@ -1,30 +1,39 @@
 syms t
-
 g = -9.81;
 v0 = 0;
 s0 = 10;
-e = 0.8
+e = 0.8;
 
-% Define x values
+% Define x values (absolute time)
 x = linspace(0, 10, 1000);
     
 % Initialize 
 y = zeros(size(x));
-
 v = int(g,t) + v0;
 s = int(v,t) + s0;
-last_t_hit = 0
+last_t_hit = 0;
 
 for i = 1:10
-    t_hit = solve(s == 0, t);
-    disp(t_hit)
+    % 1. Find roots and extract the positive local time of impact
+    t_roots = double(solve(s == 0, t));
+    t_hit_local = max(t_roots); % The ball hits the ground in the future, not the past
     
-    % Piecewise definition
-    mask = (last_t_hit < x) & (x <= t_hit);
-    y(mask) = double(subs(s, t, x(mask)));
+    % 2. Calculate the absolute time of this impact
+    t_hit_abs = last_t_hit + t_hit_local;
     
-    last_t_hit = t_hit
-    v_hit = double(subs(v, t, t_hit));
+    % 3. Piecewise definition using absolute time for the mask
+    mask = (last_t_hit <= x) & (x <= t_hit_abs);
+    
+    % 4. Substitute using local time (x - last_t_hit)
+    y(mask) = double(subs(s, t, x(mask) - last_t_hit));
+    
+    % Calculate impact velocity using the local time
+    v_hit = double(subs(v, t, t_hit_local));
+    
+    % Update the absolute time tracker for the next iteration
+    last_t_hit = t_hit_abs;
+    
+    % Generate equations for the next bounce (t resets to 0)
     v = int(g,t) - e*v_hit;    
     s = int(v,t) + 0;
 end
@@ -32,6 +41,6 @@ end
 % Plot
 plot(x, y, 'LineWidth', 2)
 grid on
-xlabel('x')
-ylabel('f(x)')
-title('Piecewise Function')
+xlabel('Time (s)')
+ylabel('Height (m)')
+title('Bouncing Ball Piecewise Function')
